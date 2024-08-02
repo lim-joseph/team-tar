f2273996abd6bf59f344cf4caf18eaa9227f2ff1
klung21 <ps6a26@gmail.com>
2024-08-03 00:58:41 +1000

    implement the title header for the create new team form

--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -1,8 +1,18 @@
+import { Input } from "@/components/ui/input"
+
 export default function Page() {
-  return <div className="text-2xl m-8">
-    Create Your New Team
-    <div className="text-base text-slate-400 mt-4">
-      hi
+  return <div>
+    <div className="bg-slate-600 text-white p-12 text-5xl">
+      Create Your New Team
     </div>
-    </div>;
+
+    <div className="text-slate-400 mt-4 ml-12">
+      Fill the form to create your team
+    </div>
+    
+    <div className="mt-8 ml-12 text-xl">
+      Team Name
+      <Input type="string" placeholder="Enter Your Team Name" className="mt-4"></Input>
+    </div>
+  </div>;
 }

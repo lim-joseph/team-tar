a6471af407f4d6c51767045b989efe4a068a35bd
joseph <limjoseph2004@gmail.com>
2024-08-02 18:39:08 +1000

    Implement root layout

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -4,9 +4,10 @@ export default function DashboardLayout({
 	children: React.ReactNode;
 }) {
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			<div className="z-10 w-full max-w-5xl items-center justify-between  text-sm lg:flex">
+		<main>
+			<div>
 				<p>dashboard layout</p>
+				{children}
 			</div>
 		</main>
 	);
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,7 @@
 export default function Dashboard() {
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			<div className="z-10 w-full max-w-5xl items-center justify-between  text-sm lg:flex">
+		<main>
+			<div>
 				<p>dashboard</p>
 			</div>
 		</main>

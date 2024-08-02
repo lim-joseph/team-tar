86063b8f5e7ea2a1242bb2073125b5be6266c2fd
yim kenneth <yimkenneth@dyn-118-139-68-199.its.monash.edu.au>
2024-08-02 19:34:54 +1000

    create the title of the page

--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -1,3 +1,8 @@
 export default function Page() {
-  return <>hi</>;
+  return <div className="text-2xl m-8">
+    Create Your New Team
+    <div className="text-base text-slate-400 mt-4">
+      hi
+    </div>
+    </div>;
 }
